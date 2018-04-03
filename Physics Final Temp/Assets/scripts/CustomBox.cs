bb74138ce2f58e0290050549d564922bdcd6ea64
Tyler <tyler.chermely@mymail.champlain.edu>
2018-04-03 16:43:18 -0400

    Initial Commit

---
+++ Physics Final Temp/Assets/scripts/CustomBox.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct BoxColliderCustom
+{
+    public float minX, maxX, minY, maxY, minZ, maxZ;
+}
